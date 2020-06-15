9494844a73030f9219d8663897944369150b30bc
academy_intern <academy_intern@academy-interns-MacBook-Air.local>
2020-06-15 09:05:14 +0200

    able to add a student

--- backend/routes/Student.js
+++ backend/routes/Student.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const con = require('../connection');
 
 //get students
-router.get('/sam',(req,res) =>{
+router.get('/',(req,res) =>{
 
     let sql = 'SELECT * FROM Student'
     console.log(sql)
@@ -25,7 +25,6 @@ router.get('/sam',(req,res) =>{
 
 //get student by id 
 router.get('/:id',(req,res)=>{
-
     var id = req.params.id
     var sql = 'SELECT * FROM Student WHERE StudentID = ?';
     con.query(sql, [id], function (err, result) {
@@ -40,47 +39,51 @@ router.get('/:id',(req,res)=>{
             })
         }
 });
-
 });
 
 
 //add a new student 
-router.post('',(req,res) =>{
 
-    
-    // let sql = 'INSERT INTO Student (StudentName, Email, Date_Registered, Date_Edited) VALUES (?,?,?,?)'
-    // con.query(sql,(err, result,fields)=> {
-    //     if(err){
-
-    //         console.log(err.message)
-    //     }else{
-    //         console.log(result)
-
-    //     }
-    // });
-        
-    
-    });
-
-
-
-
-
-//update student 
-router.patch('/:id',(req,res) => {
-    const studentID = req.params.id
-    res.status(200).json({
-        message:"updated student"
+router.post('/',(req,res) => {
+    var sql = "INSERT INTO `Student`(`StudentName`,`Email`, `Date_Registered`,`Date_Edited`) VALUES ('"+req.body.StudentName+"','"+req.body.Email+"','"+req.body.Date_Registered+"','"+req.body.Date_Edited+"');"
+    con.query(sql,function(err,result){
+        if(err){
+            console.log(err)
+        }else{
+            res.status(200).json({
+                message:'added a student',
+                result
+            })
+        }
     })
 })
 
-//Delete student
+//update student 
 router.patch('/:id',(req,res) => {
-    const studentID = req.params.id
-    res.status(200).json({
-        message:"updated student"
-    })
-});
+    // const studentID = req.params.id
+    // res.status(200).json({
+    //     message:"updated student"
+    // })
+   
+})
+
+//Delete student
+router.delete('/:id',(req,res) => {
+        var id = req.params.id
+        var sql = ' DELETE FROM `Student` WHERE StudentID = ?';
+        con.query(sql, [id], function (err, result) {
+            if(err){
+                res.status(404).json({
+                    message:`cannot delete`
+                })
+            }else{
+                res.status(200).json({
+                    message:'deleted student',
+                    result
+                })
+            }
+    });
+    });
 
 
 module.exports = router ;
